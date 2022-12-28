114f0e79ee687d7e5090ee00b06307d7c0740eb6
ohperes <peresvini2@gmail.com>
2022-12-28 03:49:36 -0300

    feat: create transactions list

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,37 @@
-import { Header } from "../../components/Header";
-import { Summary } from "../../components/Summary";
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tr>
+            <td width="50%">Desenvolvimento de site</td>
+            <td>
+              <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+            </td>
+            <td>Venda</td>
+            <td>13/04/22</td>
+          </tr>
+          <tr>
+            <td width="50%">Pizza para você</td>
+            <td>
+              <PriceHighlight variant="outcome">- R$ 32,00</PriceHighlight>
+            </td>
+            <td>Alimentação</td>
+            <td>10/04/22</td>
+          </tr>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
